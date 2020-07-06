c65c259f2e7cfe49161a93d4d6c443648fc61d67
adairvega <adairgonzalez@utez.edu.mx>
2020-07-06 17:58:59 +0200

    Implementation of flutter_swiper package https://pub.dev/packages/flutter_swiper#-readme-tab-

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Movies in theaters'),
+          //centerTitle: true, #In case of Center title required
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          ],
+        ),
         body: SafeArea(
-      child: Text('Hola Mundo'),
-    ));
+          child: Column(
+            children: <Widget>[_swiperTarjetas()],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
+        itemWidth: 200.0,
+        layout: SwiperLayout.STACK,
+      ),
+    );
   }
 }
