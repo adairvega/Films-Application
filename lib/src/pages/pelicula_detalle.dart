1bfd21f944dda895ad17698292f0f32ddbc135a7
adairvega <adairgonzalez@utez.edu.mx>
2020-07-09 17:24:21 +0200

    Adding Title, subtitle, rate and description film

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -10,6 +10,14 @@ class PeliculaDetalle extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _crearAppBar(pelicula),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 10.0),
+              _posterTitulo(context, pelicula),
+              SizedBox(),
+              _description(pelicula),
+            ]),
+          )
         ],
       ),
     );
@@ -37,6 +45,66 @@ class PeliculaDetalle extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(
+                pelicula.getPosterImg(),
+              ),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                pelicula.title,
+                style: Theme.of(context).textTheme.bodyText1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                pelicula.originalTitle,
+                style: Theme.of(context).textTheme.bodyText2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.star_border),
+                  Text(
+                    pelicula.voteAverage.toString(),
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                ],
+              )
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+
+  Widget _description(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10.0,
+        vertical: 20.0,
+      ),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
